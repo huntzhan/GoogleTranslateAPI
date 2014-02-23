803b637ed66ce364f01bf5ecd92b2e907520a981
詹昊恂 <programmer.zhx@gmail.com>
2014-02-23 18:04:24 +0800

    update setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='google_translate_api',
-    version='0.2',
+    version='0.2.1',
     author='Zhan Haoxun',
     author_email='programmer.zhx@gmail.com',
 
